ad7f4ad3c79d54cba84e76ed3a5a59862c12d3fe
Anon Yanying <kc.pp81@gmail.com>
2026-04-24 21:21:34 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+// Import Components
+import Scoreboard from "./components/Scoreboard.vue";
+import StartScreen from "./components/StartScreen.vue";
+import WeaponSelection from "./components/WeaponSelection.vue";
+import ElementSelection from "./components/ElementSelection.vue";
+import BattleArena from "./components/BattleArena.vue";
+import RoundOverScreen from "./components/RoundOverScreen.vue";
+import MatchOverScreen from "./components/MatchOverScreen.vue";
+</script>
+
+<template>
+	<div
+		class="h-dvh w-screen flex flex-col items-center justify-center p-4 relative overflow-hidden"
+	>
+		<!-- Permanent Scoreboard -->
+		<Scoreboard />
+
+		<!-- Start Screen -->
+		<StartScreen />
+
+		<!-- Select Weapon -->
+		<WeaponSelection />
+
+		<!-- Select Element -->
+		<ElementSelection />
+
+		<!-- Fighting / Result -->
+		<BattleArena />
+
+		<!-- Round Over -->
+		<RoundOverScreen />
+
+		<!-- Match Over -->
+		<MatchOverScreen />
+	</div>
+</template>
